4c351bede89b330eb6451a933fe76f86294e10b6
meilan <meilanulfia@gmail.com>
2026-03-12 12:40:43 +0700

    Memberi notifikasi dan menutup koneksi

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -34,7 +34,11 @@ namespace PraktikumADO
                 Koneksi();
                 conn.Open();
 
-               
+                MessageBox.Show("Koneksi ke database berhasil");
+
+                conn.Close();
+
+
             }
             
         }
